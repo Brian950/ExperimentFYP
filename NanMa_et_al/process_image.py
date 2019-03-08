c1c93ecc74f6d6cd453427060a05b29886be7c93
Brian950 <brianjohnheaphy@gmail.com>
2019-03-08 16:02:11 +0000

    Updates.

--- NanMa_et_al/process_image.py
+++ NanMa_et_al/process_image.py
@@ -5,21 +5,68 @@ from matplotlib import pyplot as plt
 
 SOBEL = True
 
+src = np.float32([[0, 0], [854, 0], [0, 360], [854, 360]])
+dst = np.float32([[320, 230], [550, 230], [0, 360], [854, 360]])
+img_size = (854, 360)
+
+sharp_kernel = np.array([[-1, -1, -1],
+                         [-1, 9, -1],
+                         [-1, -1, -1]])
+
+
 def process(frame, mtx=None, dist=None):
     image = frame
     image = crop(image)
 
     if mtx is not None and dist is not None:
-        image = undistort(image, mtx, dist)
+        view = undistort(image, mtx, dist)
 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    image = gray_equalisation(image)
+    transform = perspective_transform(view, src, dst, img_size)
 
-    img_size = (image.shape[1], image.shape[0])
-    print("ims1: "+str(image.shape[1])+"  ims0: "+str(image.shape[0]))
-    src = np.float32([[0, 0], [854, 0], [0, 360], [854, 360]])
-    dst = np.float32([[320, 240], [550, 240], [0, 360], [854, 360]])
-    image = perspective_transform(image, src, dst, img_size)
+    gray = cv2.cvtColor(transform, cv2.COLOR_RGB2GRAY)
+    eq_image = gray_equalisation2(gray)
+
+    sharp = sharpen(eq_image)
+
+    sobel_image = sobel(sharp, 3)
+
+    canny_image = canny(sharp, 50, 200)
+
+    binary_image = set_gray_binary(sharp)
+
+    return binary_image
+
+
+def set_gray_binary(image):
+    ret, thresh = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
+    return thresh
+
+
+def set_binary(orig):
+    # WHITE
+    img = orig
+    # Lower and upper bounds for colour values
+    white_lower = np.array([0, 0, 200])
+    white_upper = np.array([145, 60, 255])
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    mask1 = cv2.inRange(img, white_lower, white_upper)
+
+    # YELLOW
+    img = orig
+    # Lower and upper bounds for colour values
+    yellow_lower = np.array([80, 80, 200])
+    yellow_upper = np.array([255, 255, 255])
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    mask2 = cv2.inRange(img, yellow_lower, yellow_upper)
+
+    mask = cv2.bitwise_or(mask1, mask2)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    gray = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
+    (thresh, image) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
 
     return image
 
@@ -66,6 +113,7 @@ then applied to the masked array and then the masked array is used as a lookup
 to transform the original image. Noticeable 5-6 fps drop using this algorithm.
 '''
 
+
 def gray_equalisation(image):
     hist, bins = np.histogram(image.flatten(), 256, [0, 256])
 
@@ -79,7 +127,29 @@ def gray_equalisation(image):
     return image
 
 
+def gray_equalisation2(image):
+    clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(8, 8))
+    image = clahe.apply(image)
+    return image
+
+
 def perspective_transform(image, src, dst, img_size):
     matrix = cv2.getPerspectiveTransform(dst, src)
     warped = cv2.warpPerspective(image, matrix, img_size)
     return warped
+
+
+def sobel(image, kernel):
+    image = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=kernel)
+    return image
+
+
+def canny(image, lower, upper):
+    image = cv2.Canny(image, lower, upper)
+    return image
+
+
+def sharpen(image):
+    image = cv2.filter2D(image, -1, sharp_kernel)
+    image = cv2.bilateralFilter(image, 5, 50, 50)
+    return image
